1bbaec7c3b8dc58f073aeb2cb024dbe43b4e65b2
WZCSHTU <3497679187@qq.com>
2023-01-06 09:56:20 +0800

    项目更新

---
+++ src/exitWindow.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+
+import windowListener.*;
+import exitWindowListener.*;
+
+public class exitWindow {
+     //搭建退出窗口
+
+     myWindowListener listener = new myWindowListener();
+}
---
+++ src/exitWindowListener/exitWindowListener.java
@@ -0,0 +1,11 @@
+package exitWindowListener;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class exitWindowListener implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println("hello world");
+    }
+}
